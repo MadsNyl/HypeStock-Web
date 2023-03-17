65ac32a355125b3f18bbbcf92ab8f73d56b8dc39
Mads Nylund <madsnyl@gmail.com>
2023-03-17 16:11:42 +0100

    design on collapse nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,13 +7,17 @@ export default function Nav({ children }) {
     const [isActive, setActive] = useState(false);
 
     const navigation = [
-        { name: "home", path: "/" },
-        { name: "search", path: "/stock" },
-        { name: "articles", path: "/articles" },
-        { name: "reddit", path: "/reddit" },
-        { name: "exchanges", path: "/exchanges" },
-        { name: "users", path: "/users" },
-        { name: "stats", path: "/stats" },
+        { name: "home", path: "/", collapse: false },
+        { name: "tickers", path: "", collapse: true, children: [
+            { name: "search", path: "/stock", collapse: false },
+            { name: "trending", path: "/trending", collapse: false },
+            { name: "exchanges", path: "/exchanges", collapse: false },
+        ] },
+        { name: "browse", path: "", collapse: true, children: [
+            { name: "articles", path: "/articles", collapse: false },
+            { name: "reddit", path: "/reddit", collapse: false }
+        ] },
+        { name: "stats", path: "/stats", collapse: false },
     ]
 
     return (
@@ -22,21 +26,15 @@ export default function Nav({ children }) {
                 <NavLink
                     // onClick={() => { setActive(!isActive); }}
                     to={"/"}
-                    className="text-2xl font-bold text-emerald-500"
+                    className="text-3xl font-bold text-emerald-500"
                 >
                     HypeStock
                 </NavLink>
 
-                <nav className="flex items-center space-x-8">
+                <nav className="flex items-center space-x-6">
                     {
                         navigation.map((item, index) => {
-                            return <NavLink
-                                key={index}
-                                to={item.path}
-                                className="hover:text-emerald-500 uppercase text-xs"
-                            >
-                                { item.name }
-                            </NavLink>
+                            return item.collapse ? <Collapse item={item} key={index} /> : <Tab item={item} key={index} /> 
                         })
                     }
                 </nav>
@@ -51,4 +49,66 @@ export default function Nav({ children }) {
             </div>
         </div>
     )
+}
+
+const Tab = ({ item }) => {
+    return (
+        <NavLink
+            to={item.path}
+            className="hover:text-emerald-500 capitalize block font-medium"
+        >
+            { item.name }
+        </NavLink>
+    );
+}
+
+const CollapseTab = ({ item, setOpen }) => {
+    return (
+        <NavLink
+            onClick={() => {setOpen(false);}}
+            to={item.path}
+            className="py-2 px-4 rounded-lg transition duration-150 ease-in-out hover:bg-gray-100 block capitalize font-medium"
+        >
+            { item.name }
+        </NavLink>
+    );
+}
+
+const Collapse = ({ item }) => {
+
+    const [open, setOpen] = useState(false);
+
+    return (
+        <div className="relative">
+            <button
+                onClick={() => {setOpen(!open);}}
+                className="hover:text-emerald-500 capitalize font-medium flex items-center space-x-1"
+            >
+                <p>{ item.name }</p>
+                {
+                    !open
+                        ? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                            </svg>
+                        : <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                            </svg>
+                    
+                }
+            </button>
+            {
+                !open
+                    ? <></>
+                    : <div className="absolute top-8 right-0 rounded-lg bg-white border border-gray-200 shadow-md py-2 px-4">
+                        <div className="space-y-2">
+                            {
+                                item.children.map((item, index) => {
+                                    return <CollapseTab item={item} key={index} setOpen={setOpen} />
+                                })
+                            }
+                        </div>
+                    </div>
+            }
+        </div>
+    );
 }
--- src/components/home/Latest.jsx
+++ src/components/home/Latest.jsx
@@ -1,10 +1,77 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import FormatDate from "../functions/FormatDate";
+import FormatText from "../functions/FormatText";
+
+
+const Stock = ({ stock }) => {
+    return (
+        <NavLink
+            to={`/stock/${stock}`}
+            className="rounded-md bg-white py-1 border border-gray-300 transition duration-150 hover:bg-emerald-500 hover:border-emerald-500 hover:text-white"
+        >
+            <p className="text-xs text-center">
+                { stock }
+            </p>
+        </NavLink>
+    );
+}
 
 const Article = ({ article }) => {
+
+    // const stock_count = article.related_stocks.length;
+    // const stocks = article.related_stocks.slice(0, 4);
+
     return (
-        <div className="w-full py-2 rounded-md bg-white flex items-center justify-between">
+        <div className="w-full py-4 px-4 rounded-md bg-gray-50 border border-gray-200">
+            <div className="flex items-center justify-between pb-6">
+                <div>
+                    <h1 className="pb-2">
+                        { FormatText(article.title, 45) }
+                    </h1>
+                    <p className="text-xs text-emerald-500 uppercase pb-1">
+                        { article.provider }
+                    </p>
+                    <p className="text-xs text-gray-400">
+                        { FormatDate(article.created_date) }
+                    </p>
+                </div>
+            </div>
+            <div className="grid gap-2 grid-cols-6">
+                {
+                    article.related_stocks.map((stock, index) => {
+                        return <Stock key={index} stock={stock} />
+                    })
+                }
+            </div>
+        </div>
+    );
+}
+
+const Comment = ({ comment }) => {
+    return (
+        <div className="w-full py-4 px-4 rounded-md bg-gray-50 border border-gray-200">
+            <div className="flex items-center justify-between pb-6">
+                <div>
+                    <h1 className="pb-2">
+                        { FormatText(comment.body, 45) }
+                    </h1>
+                    <p className="text-xs text-emerald-500 uppercase pb-1">
+                        { comment.subdreddit }
+                    </p>
+                    <p className="text-xs text-gray-400">
+                        { FormatDate(comment.created_date) }
+                    </p>
+                </div>
+            </div>
             <div>
-                { article.provider }
+                <NavLink
+                    className="rounded-md bg-white py-1 border border-gray-300 transition duration-150 hover:bg-emerald-500 hover:border-emerald-500 hover:text-white"
+                >
+                    <p>
+                        { comment.symbol }
+                    </p>
+                </NavLink>
             </div>
         </div>
     );
@@ -18,14 +85,14 @@ export default function Latest({ articles, comments }) {
                     Latest data from social media
                 </h1>
             </div>
-            <div className="flex justify-center items-center space-x-36">
-                <div className="bg-gray-50 border border-gray-200 rounded-md py-4 px-6 max-w-md w-full">
-                    <div>
-                        <h1>
+            <div className="flex justify-center space-x-36">
+                <div className="py-4 px-6 max-w-lg w-full">
+                    <div className="pb-4">
+                        <h1 className="text-center uppercase font-bold text-xl">
                             Articles
                         </h1>
                     </div>
-                    <div>
+                    <div className="space-y-4">
                         {
                             articles?.map((article, index) => {
                                 return <Article key={index} article={article} />
@@ -33,11 +100,18 @@ export default function Latest({ articles, comments }) {
                         }
                     </div>
                 </div>
-                <div className="bg-gray-50 border border-gray-200 rounded-md py-4 px-6 max-w-md w-full">
-                    <div>
-                        <h1>
+                <div className="py-4 px-6 max-w-lg w-full">
+                    <div className="pb-4">
+                        <h1 className="text-center uppercase font-bold text-xl">
                             Reddit
                         </h1>
+                    </div>
+                    <div className="space-y-4">
+                        {
+                            comments?.map((comment, index) => {
+                                return <Comment key={index} comment={comment} />
+                            })
+                        }
                     </div>
                 </div>
             </div>
